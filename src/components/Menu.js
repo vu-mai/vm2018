90d5e01d54ba2e2bebf721f7847476b7449661af
Vu Mai <vu.mai@visma.com>
2018-06-11 00:04:41 +0200

    Connect db

---
+++ src/components/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = (props) => {
+  if (props.authenticated) {
+    return (
+      <ul className="menu">
+        <li><Link to={'/'}>To do</Link></li>
+        <li><Link to={'/completed'}>Completed</Link></li>
+        <li className="logOut"  onClick={ props.logOut }>sign out</li>
+      </ul>
+    )
+  } else {
+    return (
+      <div className="auth">
+        <form onSubmit={event => props.EmailAndPasswordAuthentication(event)}>
+          <label> Email <input type="email" ref={ props.emailInput} /> </label>
+          <label> Password <input type="password" ref={ props.passwordInput} /> </label>
+          <input type="submit" value="Register/Login" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Menu;
