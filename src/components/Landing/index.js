e2d223c9cca0fd14197197418e570e358eb14a4e
Vu Mai <vu.mai@visma.com>
2018-06-24 01:12:25 +0200

    Autosort scorebaord

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,62 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import config from '../../config';
-import { load } from '../../helpers/spreadsheet';
+import {load} from '../../helpers/spreadsheet';
 
 class LandingPage extends Component {
-    state = {
-        liste: [],
-        error: null
-    };
+  state = {
+    liste: [],
+    error: null
+  };
 
-    onLoad = (data, error) => {
-        if (data) {
-            const { liste } = data;
-            this.setState({ liste });
-        } else {
-            this.setState({ error })
-        }
-    };
-
-    initClient = () => {
-        window.gapi.client
-            .init({
-                apiKey: config.apiKey,
-                discoveryDocs: config.discoveryDocs
-            })
-            .then(() => {
-                load(this.onLoad);
-            })
-    };
-
-    componentDidMount() {
-        const { onSetUsers } = this.props;
-
-        // db.onceGetUsers().then(snapshot =>
-        //   onSetUsers(snapshot.val())
-        // );
-
-        window.gapi.load("client", this.initClient);
+  onLoad = (data, error) => {
+    if (data) {
+      const {liste} = data;
+      this.setState({liste});
+    } else {
+      this.setState({error})
     }
+  };
 
-    render() {
-        const { users } = this.props;
-        const { liste, error } = this.state;
+  initClient = () => {
+    window.gapi.client.init({apiKey: config.apiKey, discoveryDocs: config.discoveryDocs}).then(() => {
+      load(this.onLoad);
+    })
+  };
 
-        return (
-            <div>
-                <h1>VM 2018</h1>
+  componentDidMount() {
+    const {onSetUsers} = this.props;
 
-                <iframe
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhe7u8wxjCREKwjZfOaYMWWgyAdaZUunq0BYTdDkW_p9ipejuMU10hpcPaKMWEPkutBChfySpH6yrh/pubhtml?widget=true&amp;headers=false"
-                    height="520"
-                    width="1040"
-                    allowfullscreen=""
-                    frameborder="0"
-                >
-                </iframe>
-            </div>
-        );
-    }
+    // db.onceGetUsers().then(snapshot =>
+    //   onSetUsers(snapshot.val())
+    // );
+
+    window.gapi.load("client", this.initClient);
+  }
+
+  render() {
+    const {users} = this.props;
+    const {liste, error} = this.state;
+
+    return (<div>
+      <h1>VM 2018</h1>
+
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQcgeOeNnBHFCxe9mksaecAdbHq-D25IzllECaIAgGYng6qcNLotJD9yzERCYCanoCPTzbkb41pJlgO/pubhtml?gid=1979797024&amp;widget=true&amp;headers=false"
+        height="520"
+        width="1040"
+        allowfullscreen=""
+        frameborder="0"
+        scrolling="no"
+      />
+    </div>);
+  }
 }
 
 export default LandingPage;
